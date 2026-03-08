45fbf142ac5267e5f754c1da14b614b1d7f84c6c
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-08 12:59:46 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,8 +1,20 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/infinix/Infinix-X657
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# System as Root (SAR)
+PRODUCT_PACKAGES += \
+    linker \
+    linker.vendor
+
+# No A/B (seamless unsupported)
+PRODUCT_PACKAGES += \
+    otapreopt_script
