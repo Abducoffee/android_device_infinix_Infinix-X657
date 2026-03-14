17f0d37095c00346b6e7e819eb166f33e0aa46fd
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-14 21:26:45 +0300

    Update and rename omni_Infinix-X657.mk to twrp_Infinix-X657.mk

---
+++ twrp_Infinix-X657.mk
@@ -0,0 +1,16 @@
+# File: twrp_Infinix-X657.mk
+
+# Inherit base - NO 64-bit (MT6580 is 32-bit only)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit device config
+$(call inherit-product, device/infinix/Infinix-X657/device.mk)
+
+# Inherit TWRP config
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_DEVICE := Infinix-X657
+PRODUCT_NAME := twrp_Infinix-X657
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X657
+PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
