ebd9ff59860163c5f6ab7c264e40b35afba13f2f
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-14 22:06:59 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,8 +2,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_Infinix-X657.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_Infinix-X657-user \
-    twrp_Infinix-X657-userdebug \
-    twrp_Infinix-X657-eng
