a2a2b18344f1904325fac968f9f2949a04622940
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-14 22:48:23 +0300

    Update and rename twrp_Infinix-X657.mk to twrp_X657.mk

--- twrp_X657.mk
+++ twrp_X657.mk
@@ -1,16 +1,11 @@
-# File: twrp_Infinix-X657.mk
+# File: twrp_X657.mk
 
-# Inherit base
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Inherit device config
 $(call inherit-product, device/infinix/Infinix-X657/device.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit TWRP config
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-PRODUCT_DEVICE := Infinix-X657
-PRODUCT_NAME := twrp_Infinix-X657
+PRODUCT_DEVICE := X657
+PRODUCT_NAME := twrp_X657
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X657
 PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
