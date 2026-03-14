bb24a9e27ac4e469ac08406bcd0b3c7b6e9cce3f
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-14 23:25:25 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,23 +1,35 @@
-# File: device.mk
-
 LOCAL_PATH := device/infinix/Infinix-X657
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# API Level (Android 10 = API 29)
+# API Level (Android 10 = 29)
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-# Fastbootd
+# Fastbootd (required for dynamic partitions)
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# Additional binaries
+# Additional libraries
 PRODUCT_PACKAGES += \
     libion \
     libhidltransport \
     libhwbinder
+
+# Encryption libraries
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# HIDL packages needed for recovery
+PRODUCT_PACKAGES += \
+    libkeymaster4 \
+    libkeymaster41
+
+# tzdata
+PRODUCT_PACKAGES += \
+    tzdata_twrp
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
