ff5d51f3f951425d7687b411669250e6f7b45d53
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-14 23:22:22 +0300

    Update twrp_X657.mk

--- twrp_X657.mk
+++ twrp_X657.mk
@@ -1,9 +1,13 @@
-# File: twrp_X657.mk
-
+# Inherit base
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit device config
 $(call inherit-product, device/infinix/Infinix-X657/device.mk)
+
+# Inherit TWRP config (twrp-11.0 uses vendor/omni)
 $(call inherit-product, vendor/omni/config/common.mk)
 
+# Device identifiers
 PRODUCT_DEVICE := X657
 PRODUCT_NAME := twrp_X657
 PRODUCT_BRAND := Infinix
