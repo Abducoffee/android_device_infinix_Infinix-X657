59770f7fe61530837b9258e979727b038c01a3d2
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-15 20:22:02 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,6 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 150
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_EXCLUDE_ENCRYPTED_BACKUPS := true
 
 # =============================================
 # Touch / Input
