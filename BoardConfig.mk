6398f36ffdbf3a83e1c280785f53d28916c36487
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-15 19:52:28 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,8 @@
 
 # Force LZMA compression for maximum space saving
 BOARD_RAMDISK_USE_LZMA := true
+LZMA_RAMDISK_TARGETS := recovery
+BOARD_COMPRESS_RAMDISK := true
 
 DEVICE_PATH := device/infinix/X657
 
