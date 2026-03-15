363a0f696d96d4045d4c8cb789a797db76f7727c
Abducoffee <155775049+Abducoffee@users.noreply.github.com>
2026-03-15 12:48:32 +0300

    Update twrp_X657.mk

--- twrp_X657.mk
+++ twrp_X657.mk
@@ -1,9 +1,6 @@
 # Inherit base
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit device config
-$(call inherit-product, device/infinix/X657/device.mk)
-
 # Inherit TWRP config (twrp-11.0 uses vendor/omni)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -13,3 +10,39 @@ PRODUCT_NAME := twrp_X657
 PRODUCT_BRAND := infinix
 PRODUCT_MODEL := X657
 PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
+
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# API Level (Android 10 = 29)
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Fastbootd (required for dynamic partitions)
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Additional libraries
+PRODUCT_PACKAGES += \
+    libion \
+    libhidltransport \
+    libhwbinder
+
+# Encryption libraries
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# HIDL packages needed for recovery
+PRODUCT_PACKAGES += \
+    libkeymaster4 \
+    libkeymaster41
+
+# tzdata
+PRODUCT_PACKAGES += \
+    tzdata_twrp
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
